40aecb83262b40e238ae9519799686bc5a604932
Jairo Velasco <jairove7@gmail.com>
2017-01-25 17:30:19 +0100

    Arreglos en el posicionamiento de widgets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NavbarComponent} from './navbar/navbar.component';
 import {TodoComponent} from './todo/todo.component';
 import {QuotesComponent} from './quotes/quotes.component';
+import {DragulaService} from 'ng2-dragula';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,26 @@ import {QuotesComponent} from './quotes/quotes.component';
 })
 export class AppComponent {
   //La lista de widgets deberia recuperarse de la db y contener el orden de los widgets
-  widgets = [QuotesComponent, TodoComponent];
-  constructor() {
+  widgets = [QuotesComponent, TodoComponent, TodoComponent];
+
+ public many: Array<string> = ['The', 'possibilities', 'are', 'endless!'];
+
+  constructor(private dragulaService: DragulaService) {
+    dragulaService.dropModel.subscribe((value) => {
+      this.onDropModel(value.slice(1));
+    });
+    dragulaService.removeModel.subscribe((value) => {
+      this.onRemoveModel(value.slice(1));
+    });
+  }
+
+  private onDropModel(args) {
+    let [el, target, source] = args;
+    // do something else
+  }
+
+  private onRemoveModel(args) {
+    let [el, source] = args;
+    // do something else
   }
 }
