1f48ce6577d38d60afb8c5b41ee0cd3bb866c777
Jairo Velasco <jairove7@gmail.com>
2017-01-25 17:46:21 +0100

    Añadido soporte para múltiples tamaños de widget. Comprobado funcionamiento del array de widgets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,26 +11,9 @@ import {DragulaService} from 'ng2-dragula';
 })
 export class AppComponent {
   //La lista de widgets deberia recuperarse de la db y contener el orden de los widgets
-  widgets = [QuotesComponent, TodoComponent, TodoComponent];
+  public widgets = [{type: QuotesComponent, colSize: "col-xs-4"},{type: QuotesComponent, colSize: "col-xs-4"},{type: TodoComponent, colSize: "col-xs-8"},{type: TodoComponent, colSize: "col-xs-4"}];
 
- public many: Array<string> = ['The', 'possibilities', 'are', 'endless!'];
-
-  constructor(private dragulaService: DragulaService) {
-    dragulaService.dropModel.subscribe((value) => {
-      this.onDropModel(value.slice(1));
-    });
-    dragulaService.removeModel.subscribe((value) => {
-      this.onRemoveModel(value.slice(1));
-    });
+  constructor() {
   }
 
-  private onDropModel(args) {
-    let [el, target, source] = args;
-    // do something else
-  }
-
-  private onRemoveModel(args) {
-    let [el, source] = args;
-    // do something else
-  }
 }
